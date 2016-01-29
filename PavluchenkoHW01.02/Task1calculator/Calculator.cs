356dc6e6fcff8565b2709e3cb8e24b88a8b4dca0
Pikovit44 <Pikovit44@gmail.com>
2016-01-29 15:55:20 +0200

    FirstTaskV1

---
+++ PavluchenkoHW01.02/Task1calculator/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1calculator
+{
+    static public class Calculator
+    {
+
+
+        public static void Addition (int first, int second)
+        {
+            
+            Console.WriteLine("\nAnswer: ");
+            Console.WriteLine ( first + second);
+           
+        }
+
+        public static void Subtraction(int first, int second)
+        {
+            Console.WriteLine("\nAnswer: ");
+            Console.WriteLine(first - second);
+        }
+
+        public static void Multiplication(int first, int second)
+        {
+            Console.WriteLine("\nAnswer: ");
+            Console.WriteLine( (float)first * second );
+        }
+
+        public static void Division(int first, int second)
+        {
+            Console.WriteLine("\nAnswer: ");
+            Console.WriteLine((float)first / second);
+        }
+
+
+    }
+}
---
+++ PavluchenkoHW01.02/Task1calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = Helper.EnterInt("Number 1");
+            Console.WriteLine("Select a mathematical operation: \n1)+\n2)-\n3)*\n4)\\");
+            int operation = Helper.EnterMenu("menu");
+            int secondNum = Helper.EnterInt("Number 2");
+
+            switch (operation)
+            {
+                case 1:
+                    Calculator.Addition(firstNum, secondNum);
+                    break;
+                case 2:
+                    Calculator.Subtraction(firstNum, secondNum);
+                    break;
+                case 3:
+                    Calculator.Multiplication(firstNum, secondNum);
+                    break;
+                case 4:
+                    Calculator.Division(firstNum, secondNum);
+                    break;
+                default:
+                    break;
+            }
+
+            
+            
+            
+            
+        }
+    }
+}

33015f885c3c6f264aab19adfeb33d22ebe6e7b0
Pikovit44 <Pikovit44@gmail.com>
2016-02-08 15:02:19 +0200

    Task1,2,3,4,5,7

---
+++ PavluchenkoHW08.02/Task7MyArrayList/MyArrayList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7MyArrayList
+{
+    public class MyArrayList
+    {
+        object[] array;
+
+        public MyArrayList()
+        {
+            object[] arr = new object[0];
+            array = arr;
+        }
+
+        int count = 0;
+        int capacity = 0;
+
+        public int Capacity
+        {
+            get { return capacity; }
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public void Add<T>(T el)
+        {
+            count++;
+
+            if (capacity == 0)
+                capacity = 2;
+            else capacity *= capacity;
+
+            Array.Resize(ref array, capacity);
+            
+            array[count - 1] = (object)el;
+        }
+
+        public object this[int index]
+        {
+            get { return array[index]; }
+
+        }
+
+
+    }
+
+}
---
+++ PavluchenkoHW08.02/Task7MyArrayList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7MyArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArrayList first = new MyArrayList();
+            first.Add(5);
+            first.Add("test");
+            first.Add(true);
+
+            Console.WriteLine(first.Capacity);
+            Console.WriteLine(first.Count);
+
+            Console.WriteLine(first[2]);
+            Console.WriteLine(first[2].GetType()); 
+
+
+        }
+    }
+}

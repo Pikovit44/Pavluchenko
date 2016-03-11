82dd4afb548cb8ace5234baf77c0755b01a68db1
Pikovit44 <Pikovit44@gmail.com>
2016-03-11 22:52:58 +0200

    NUnit tests started

---
+++ ISDhomeWorks/LittleFirTree/NunitTests/FirTreeTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FirTree;
+using Enums;
+
+namespace NunitTests
+{
+    [TestFixture]
+    public class FirTreeTests
+    {
+        [SetUp]
+        public void Init()
+        {
+            Forest forest = new Forest(PartOfYear.Winter);
+            forest.FirTreeBorn();
+        }
+
+        [Test]
+        public void FirTreeInfo()
+        {
+            Forest forest = new Forest(PartOfYear.Winter);
+            forest.FirTreeBorn();
+        }
+    }
+}

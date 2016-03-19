d4f0a00ba957ca2f4255776e858ad1aa94192794
Pikovit44 <Pikovit44@gmail.com>
2016-03-19 14:32:12 +0200

    Code first is working! yeah! it's really cool thing!

---
+++ Training/CodeFirst/CodeFirst/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var cntx = new SchoolContext())
+            {
+                Student stud = new Student { StudentName = "New Student" };
+
+                cntx.Students.Add(stud);
+                cntx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Training/Linq/Linq1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, -2, 3, 0, -4, 5, 12, 34, 2, 4 };
+            var posNums = from n in nums
+                          where n > 0
+                          where n > 2
+                          orderby n descending
+                          select Math.Sqrt(n) ;
+            Console.WriteLine("Положительные значения меньше 10 из массива nums: ");
+
+            foreach (int i in posNums)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}

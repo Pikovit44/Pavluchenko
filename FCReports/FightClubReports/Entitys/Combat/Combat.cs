db4075f329412c7c3f1f8cbdb0368075fc570317
Pikovit44 <Pikovit44@gmail.com>
2016-03-23 17:17:10 +0200

    Diagram v0.2, + prop name

--- FCReports/FightClubReports/Entitys/Combat/Combat.cs
+++ FCReports/FightClubReports/Entitys/Combat/Combat.cs
@@ -6,7 +6,7 @@ namespace FightClubReports
     public class Combat
     {
         public int Id { get; set; }
-        public DateTime TimeOfStartCombat { get; set; }
+        public DateTime TimeOfStart { get; set; }
         public string Type { get; set; } // pvp or pve 
         public List<RoundLog> Log { get; set; }
         public Player Player1 { get; set; }
